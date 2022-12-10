00cdbaea520dc8fe77ad7e10321cb5f76388fd63
FelipeFa6 <felipe.farias.e1@gmail.com>
2022-12-10 00:06:08 -0300

    Updated permissions

--- Dashboard/utils/connection.php
+++ Dashboard/utils/connection.php
@@ -2,9 +2,9 @@
 function create_connection(){
 
   $db_host      = '127.0.0.1';
-  $db_username  = "root";
-  $db_password  = "";
-  $db_name      = "EasyViajes";
+  $db_username  = 'root';
+  $db_password  = 'adm';
+  $db_name      = 'EasyViajes';
 
   try {
     $conn = mysqli_connect($db_host, $db_username, $db_password, $db_name);
